bea04a96fc3ed24d5a7310c0233d4e9d8a375c43
Lê Huân <lebaohuan25398@gmail.com>
2023-09-30 21:18:11 +0700

    add products

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -10,6 +10,7 @@ const authStore = useAuthStore();
             <div class="div-nav-left">
                 <router-link to="/" class="nav-item nav-link">Home</router-link>
                 <router-link to="/users" class="nav-item nav-link">Users</router-link>
+                <router-link to="/products" class="nav-item nav-link">Products</router-link>
             </div>
             <div class="div-nav-right">
                 <button @click="authStore.logout()" class="btn btn-link nav-item nav-link">Logout</button>
