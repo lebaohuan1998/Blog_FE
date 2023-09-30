ae0f1f39f6e4ebf45e05b3e70bdeb32dbe7b0c90
Lê Huân <lebaohuan25398@gmail.com>
2023-09-30 19:09:20 +0700

    commit file

---
+++ src/components/Nav.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useAuthStore } from '@/stores';
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+    <nav v-show="authStore.user" class="navbar navbar-expand navbar-dark bg-dark">
+        <div class="navbar-nav" style="width: 100%;display: block;">
+            <div class="div-nav-left">
+                <router-link to="/" class="nav-item nav-link">Home</router-link>
+                <router-link to="/users" class="nav-item nav-link">Users</router-link>
+            </div>
+            <div class="div-nav-right">
+                <button @click="authStore.logout()" class="btn btn-link nav-item nav-link">Logout</button>
+            </div>
+        </div>
+    </nav>
+</template>
